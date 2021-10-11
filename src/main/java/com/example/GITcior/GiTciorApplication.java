f015f12abf86c32580b72fef5e44151607fabcb7
pshemo <pshemyslav10@gmail.com>
2021-10-11 21:17:43 +0200

    Komit

---
+++ src/main/java/com/example/GITcior/GiTciorApplication.java
@@ -0,0 +1,13 @@
+package com.example.GITcior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiTciorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiTciorApplication.class, args);
+	}
+
+}
